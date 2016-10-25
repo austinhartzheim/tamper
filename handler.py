a5d26cab874133baf74aac57382965706d8e6a47
Austin Hartzheim <inbox@austinhartzheim.me>
2016-10-25 23:05:13 +0000

    Made some modifications to the default handlers.

--- handler.py
+++ handler.py
@@ -1,17 +1,17 @@
 import json
+import time
 
 def hello(event, context):
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "timestamp": time.time(),
+        "event": event
     }
 
-    response = {
+    return {
         "statusCode": 200,
-        "body": json.dumps(body)
-    };
-
-    return response
+        "body": json.dumps(body, indent=2)
+    }
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY integration
     """
